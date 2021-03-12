3854268d25b4800cb550be69e4d56bc46e45c220
Reid <Reid.Finn@yahoo.com>
2021-03-11 20:02:12 -0600

    buttons working with event listeners

--- Covid_tracker_JS_Frontend/index.js
+++ Covid_tracker_JS_Frontend/index.js
@@ -4,33 +4,41 @@ let new_death = document.getElementById("new_death");
 let total_death = document.getElementById("total_death");
 let total_recovered = document.getElementById("total_recovered");
 let total_cases = document.getElementById("total_cases");
-let table = document.getElementById('state_stat')
-let state = document.querySelector('.map svg-content-responsive')
- 
-function makeButtons(){
-	fetch("https://covid-19-usa-data-by-zt.p.rapidapi.com/GetUSStateWiseData", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "c040d6ad58msh0dac702e285eb0bp1190d7jsnd7236d84ebe9",
-		"x-rapidapi-host": "covid-19-usa-data-by-zt.p.rapidapi.com"
-		}
-	})
 
+ 
+
+document.addEventListener("DOMContentLoaded", () => {
+	makeButtons();
+})
+
+function makeButtons(){
+    fetch("https://covid-19-usa-data-by-zt.p.rapidapi.com/GetUSStateWiseData", {
+    "method": "GET",
+    "headers": {
+        "x-rapidapi-key": "c040d6ad58msh0dac702e285eb0bp1190d7jsnd7236d84ebe9",
+        "x-rapidapi-host": "covid-19-usa-data-by-zt.p.rapidapi.com"
+        }
+    })
 .then(response => response.json())
 .then(states =>{
     //debugger
-	console.log(states)
+    console.log(states)
     states.data.forEach(state =>{
-	   const stateButtonString = `<button id=${state.code}> ${state.code} </button>`
-	   document.querySelector('.buttons').innerHTML += stateButtonString
-		state.onload = function(){
-			
-		}
-	
-		
-	})
+       const stateButtonString = `<button id=${state.code} class= "state"> ${state.code} </button>`
+       document.querySelector('.buttons').innerHTML += stateButtonString
+       
+    })
+	let array = Array.from(document.getElementsByClassName(`state`))
+	array.forEach(button => button.addEventListener("click", (e)=>{console.log("added event")
+	}))
 })
-
+}
+// Listeners here
+function getCovidData() {
+	const stateButtons = document.getElementsByClassName("buttons")[0].children
+	   stateButtons.forEach(button =>{
+		   button.addEventListener("click", ()=>{console.log("added event")})
+	   })
 }
 // let state_data = fetch("https://covid-19-usa-data-by-zt.p.rapidapi.com/GetUSStateWiseData", {
 // 	"method": "GET",
@@ -59,20 +67,7 @@ function makeButtons(){
 // })
 
 
-// Listeners here
-function addButtonListeners() {
-	const stateButtons = document.getElementsByClassName("buttons")[0].children
-	   stateButtons.forEach(button =>{
-		   button.addEventListener("click", (e)=>{console.log("added event")})
-	   })
-}
 
-document.addEventListener("DOMContentLoaded", () =>{
-	makeButtons();
-	debugger
-
-	
-})
 //console.log('#state node type is', state)
 
 //document.querySelectorAll('iframe').forEach( item =>
